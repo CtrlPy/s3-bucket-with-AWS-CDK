d14ca4b17b181f3141a8541e2701542c40eeacc8
ctrlpy <ctrlpy3@gmail.com>
2024-03-01 16:08:47 +0200

    firts comit

---
+++ medium_awscdk/__init__.py
@@ -0,0 +1,9 @@
+from aws_cdk import Stack, aws_s3 as s3
+from constructs import Construct
+
+
+class MediumAwscdkStack(Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        s3.Bucket(self, id="bucket123", bucket_name="sharmi-bucket123")
